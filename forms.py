7d294db24441bb2538f8a338e7a7c9bec8e135bb
bart3br <95648151+bart3br@users.noreply.github.com>
2023-06-19 15:41:50 +0200

    added Login Form, secret key and flash message

--- forms.py
+++ forms.py
@@ -1,12 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, email_validator
 
 class RegistrationForm(FlaskForm):
     #creating a form for registration, defining the fields and validators
-    #username field, can't be empty, length between 2 and 25
+    #we need username, email, password, confirm password and submit button for user registration
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    #creating a form for user login, defining the fields and validators
+    #here we only need email, password and submit button since we don't create a new user here
+    #also added a remember me checkbox to remember the user with browser cookies
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-intakes = [
+#randomly generated 16 byte secret key (by running secrets.token_hex in terminal)
+app.config['SECRET_KEY'] = 'cd427c1e4b942503f56b028cea251a64'
+
+invoices = [
     {
         'author': 'Bartosz Rodowicz',
         'name': 'Faktura 1',
@@ -21,11 +25,26 @@ intakes = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', intakes=intakes)
+    return render_template('home.html', invoices=invoices)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='abcd')
+    return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    
+    #displaying a flash message after successful registration
+    if form.validate_on_submit():
+        flash(f'Successfully created an account!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 #windows powershell
 #$env:FLASK_APP = "main.py"
