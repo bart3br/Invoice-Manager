1e17f7d13cca8bfc98165e71c0ec5e9f532e9e07
bart3br <95648151+bart3br@users.noreply.github.com>
2023-06-19 13:59:46 +0200

    user registration class

--- forms.py
+++ forms.py
@@ -1,4 +1,12 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
-    pass
+    #creating a form for registration, defining the fields and validators
+    #username field, can't be empty, length between 2 and 25
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    
