45451aab562459a7db3c8f3c0f5220dd54d4e82f
bart3br <95648151+bart3br@users.noreply.github.com>
2023-06-20 20:33:58 +0200

    added change username and email mechanics

--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Email, Length, EqualTo, email_validator
 from application.models import User
@@ -32,3 +33,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+class UpdateAccountForm(FlaskForm):
+    #creating a form for updating user account
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            #custom validator to check if the username already exists in the database
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is already in use. Try a different one.')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            #custom validator to check if the email already exists in the database
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email is already in use. Try a different one.')
